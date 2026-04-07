ed376439643816363b743d203f1b79821b73cfc3
Horacio Sierra <118361694+HoracioSierra@users.noreply.github.com>
2026-04-07 17:48:30 -0400

    Add ESLint configuration for JavaScript files

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["code.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "script",
+      globals: {
+        ...globals.browser
+      }
+    }
+  }
+];
